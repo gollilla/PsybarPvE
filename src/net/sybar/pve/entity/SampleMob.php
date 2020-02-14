ceee364dd6d25f3d9486988a0e9145b160c50da5
soradore <soradore@svk.jp>
2020-02-15 03:10:35 +0900

    Add files via upload

--- src/net/sybar/pve/entity/SampleMob.php
+++ src/net/sybar/pve/entity/SampleMob.php
@@ -19,17 +19,17 @@ class SampleMob extends Human {
     public function setSkin(){
         $img = @imagecreatefrompng( $this->getDataFolder() . "images/sample.png" );
         $skinbytes = "";
-		$s = (int)@getimagesize($path)[1];
-		for($y = 0; $y < $s; $y++){
-			for($x = 0; $x < 64; $x++){
-				$colorat = @imagecolorat($img, $x, $y);
-				$a = ((~((int)($colorat >> 24))) << 1) & 0xff;
-				$r = ($colorat >> 16) & 0xff;
-				$g = ($colorat >> 8) & 0xff;
-				$b = $colorat & 0xff;
-				$skinbytes .= chr($r) . chr($g) . chr($b) . chr($a);
-			}
-		}
+	    $s = (int)@getimagesize($path)[1];
+        for($y = 0; $y < $s; $y++){
+            for($x = 0; $x < 64; $x++){
+                $colorat = @imagecolorat($img, $x, $y);
+                $a = ((~((int)($colorat >> 24))) << 1) & 0xff;
+                $r = ($colorat >> 16) & 0xff;
+                $g = ($colorat >> 8) & 0xff;
+                $b = $colorat & 0xff;
+                $skinbytes .= chr($r) . chr($g) . chr($b) . chr($a);
+            }
+        }
         @imagedestroy($img);
         $this->setSkin(new Skin("sample.skin", $skinbytes, "", "geometry.sample", ""));
         $this->sendSkin();
