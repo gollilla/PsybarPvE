4b06b372908894623660de7f7d039a61a666a807
soradore <soradore@svk.jp>
2020-02-15 03:07:53 +0900

    new

---
+++ src/net/sybar/pve/main.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace net\sybar\pve;
+
+use pocketmine\plugin\PluginBase;
+
+class main extends PluginBase {
+
+    public function onEnable(){
+
+    }
+
+    public function onDisable(){
+        
+    }
+}
+
