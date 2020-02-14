e3ae5def5a399929a6a7b5fcb281b24c69408810
soradore <soradore@svk.jp>
2020-02-15 03:09:33 +0900

    Add files via upload

---
+++ src/net/sybar/pve/entity/SampleMob.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace net\sybar\pve\entity;
+
+use pocketmine\entity\Entity;
+use pocketmine\entity\Human;
+
+class SampleMob extends Human {
+    
+    private $entityLevel;
+    private $isBoss = false;
+    private $target;
+
+    public function __construct(){
+
+    }
+
+
+    public function setSkin(){
+        $img = @imagecreatefrompng( $this->getDataFolder() . "images/sample.png" );
+        $skinbytes = "";
+		$s = (int)@getimagesize($path)[1];
+		for($y = 0; $y < $s; $y++){
+			for($x = 0; $x < 64; $x++){
+				$colorat = @imagecolorat($img, $x, $y);
+				$a = ((~((int)($colorat >> 24))) << 1) & 0xff;
+				$r = ($colorat >> 16) & 0xff;
+				$g = ($colorat >> 8) & 0xff;
+				$b = $colorat & 0xff;
+				$skinbytes .= chr($r) . chr($g) . chr($b) . chr($a);
+			}
+		}
+        @imagedestroy($img);
+        $this->setSkin(new Skin("sample.skin", $skinbytes, "", "geometry.sample", ""));
+        $this->sendSkin();
+    }
+}
