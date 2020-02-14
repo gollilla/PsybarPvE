65585be93420b754436c6e8a1e821eb177a7158a
soradore <soradore@svk.jp>
2020-02-15 03:11:09 +0900

    Add files via upload

--- src/net/sybar/pve/entity/SampleMob.php
+++ src/net/sybar/pve/entity/SampleMob.php
@@ -19,7 +19,7 @@ class SampleMob extends Human {
     public function setSkin(){
         $img = @imagecreatefrompng( $this->getDataFolder() . "images/sample.png" );
         $skinbytes = "";
-	    $s = (int)@getimagesize($path)[1];
+        $s = (int)@getimagesize($path)[1];
         for($y = 0; $y < $s; $y++){
             for($x = 0; $x < 64; $x++){
                 $colorat = @imagecolorat($img, $x, $y);
