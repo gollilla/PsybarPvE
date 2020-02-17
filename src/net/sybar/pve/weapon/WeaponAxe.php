b8e0e541409dc73d7e9562cd80d9c89d18d89846
soradore <soradore@svk.jp>
2020-02-17 21:09:14 +0900

    add axe

---
+++ src/net/sybar/pve/weapon/WeaponAxe.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace net\sybar\pve\weapon;
+
+use pocketmine\item\Axe;
+
+abstract class WeaponAxe extends Axe implements Weapon
+{
+    /** @var int */
+    public $xp = 0;
+    /** @var float */
+    private $baseAttackPoint = 5.0;
+
+    public function __construct($id, $meta)
+    {
+        parent::__construct($id, $meta, $this->getWeaponName());
+        $this->setUnbreakable();
+    }
+
+    public function getXp(): int
+    {
+        return $this->xp;
+    }
+
+    public function calcLevel()
+    {
+        $xp = $this->getXp();
+    }
+
+    public function addXp(int $xp)
+    {
+        $this->xp += $xp;
+    }
+
+    public function getWeaponName(): string
+    {
+        return "";
+    }
+
+    public function getWeaponId(): int
+    {
+        return -1;
+    }
+
+    public function getAttackPoints(): float
+    {
+        return $this->baseAttackPoint;
+    }
+
+    public function setAttackPoints(int $point)
+    {
+        $this->baseAttackPoint = $point;
+    }
+}
