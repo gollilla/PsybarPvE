68f5866e0fd282753de495b1941e2183dfee0078
soradore <soradore@svk.jp>
2020-02-18 21:47:09 +0900

    add a lot of contents

--- src/net/sybar/pve/main.php
+++ src/net/sybar/pve/main.php
@@ -3,11 +3,21 @@
 namespace net\sybar\pve;
 
 use pocketmine\plugin\PluginBase;
+use net\sybar\pve\weapon\WeponFactory;
 
 class main extends PluginBase {
 
-    public function onEnable(){
+    public static $instance = null;
 
+    public function onEnable(){
+        WeponFactory::init();
+        self::$instance = $this;
+        $this->saveResource("exp_tbl.json");
+    }
+
+    public static function getInstance(): PluginBase
+    {
+        return self::$instance;
     }
 
     public function onDisable(){
--- src/net/sybar/pve/weapon/WeaponSword.php
+++ src/net/sybar/pve/weapon/WeaponSword.php
@@ -2,29 +2,35 @@
 namespace net\sybar\pve\weapon;
 
 use pocketmine\item\Sword;
+use net\sybar\pve\weapon\exp\ExpTbl;
 
 abstract class WeaponSword extends Sword implements Weapon
 {
+
     /** @var int */
     public $xp = 0;
     /** @var float */
     private $baseAttackPoint = 5.0;
 
-    public function __construct($id, $meta)
+    public function __construct($id = self::SWORD, $meta = 0)
     {
         parent::__construct($id, $meta, $this->getWeaponName());
         $this->setUnbreakable();
+        $this->exp_tbl = new ExpTbl($this);
     }
+
 
     public function getXp(): int
     {
         return $this->xp;
     }
 
-    public function calcLevel()
+
+    public function getWeaponLevel(): int
     {
-        $xp = $this->getXp();
+        return $this->getNamedTag()->getCompound(self::TAG_INFO)->getInt(self::TAG_INFO_LEVEL);
     }
+
 
     public function addXp(int $xp)
     {
